e9972f7b67801082dd99f3b4d96ae08af870dcfd
annc19324 <annc19324@gmail.com>
2024-10-30 18:42:35 +0700

    update

---
+++ src/eventmanagementd17cnpm1/EventManagementD17CNPM1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eventmanagementd17cnpm1;
+
+/**
+ *
+ * @author annc1
+ */
+public class EventManagementD17CNPM1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
